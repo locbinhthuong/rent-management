868ac5fa66b6cbd58e0648976baab3caf8a1189b
locbinhthuong <tanlocdepzai123@gmail.com>
2026-05-27 16:39:29 +0700

    fix: add cloudinary cloud name to next.config to prevent app crash

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: 'dzwohq6wl',
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      }
+    ],
+  },
 };
 
 export default nextConfig;
